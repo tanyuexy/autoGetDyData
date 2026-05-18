bad628120c4d4df60cf3e2936cd9c18f4622d6bc
xyTuoren <2895845213@qq.com>
2026-05-18 23:14:59 +0800

    合并文件

--- scripts/douyin-creator/publish/utils.js
+++ scripts/douyin-creator/publish/utils.js
@@ -1,5 +1,21 @@
 const path = require("path");
-const { parseArgs } = require("./parse-args");
+
+function parseArgs(argv) {
+  const result = {};
+  for (let i = 0; i < argv.length; i += 1) {
+    const token = argv[i];
+    if (!token.startsWith("--")) continue;
+    const key = token.slice(2);
+    const next = argv[i + 1];
+    if (next && !next.startsWith("--")) {
+      result[key] = next;
+      i += 1;
+    } else {
+      result[key] = true;
+    }
+  }
+  return result;
+}
 const { PUBLISH_DEBUG_DIR, saveDebugArtifacts } = require("./debug");
 const { waitVisible, setTextLikeInput } = require("./dom");
 const { fillTitleAndDescription } = require("./editor");
